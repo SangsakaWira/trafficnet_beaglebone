95119e040fedea1775663e3723abedb85e30ba39
Sangsaka Wira <sangsakawira@gmail.com>
2019-06-26 20:48:30 +0700

    coba lampu

--- traffic.py
+++ traffic.py
@@ -2,7 +2,7 @@ import requests
 import time
 import Adafruit_BBIO.GPIO as GPIO
 
-output_pins = ["P8_7","P8_8","P8_9","P8_10","P8_11"]
+output_pins = ["P8_7","P8_8","P8_9","P8_10","P8_11","P8_12","P8_14","P8_16","P8_18","P9_49","P9_117","P9_115"]
 lampu1 = 0
 lampu2 = 0
 lampu3 = 0
@@ -19,23 +19,28 @@ while(True):
 	lampu3 = r.json()["lampu3"]
 	lampu4 = r.json()["lampu4"]
 
-	if(lampu1 == "1"):
-		GPIO.output(output_pins[0],GPIO.HIGH)
-		GPIO.output(output_pins[1],GPIO.HIGH)
-		GPIO.output(output_pins[2],GPIO.HIGH)
-		GPIO.output(output_pins[3],GPIO.HIGH)
-	elif(lampu1 == "2"):
-		GPIO.output(output_pins[4],GPIO.HIGH)
-		GPIO.output(output_pins[5],GPIO.HIGH)
-		GPIO.output(output_pins[1],GPIO.HIGH)
-		GPIO.output(output_pins[0],GPIO.HIGH)
-	elif(lampu1 == "3"):
-		GPIO.output(output_pins[0],GPIO.HIGH)
-		GPIO.output(output_pins[0],GPIO.HIGH)
-		GPIO.output(output_pins[0],GPIO.HIGH)
-		GPIO.output(output_pins[0],GPIO.HIGH)
-	else:
-		print("merah")
+	for i in output_pins:
+		GPIO.output(i,GPIO.HIGH)
+
+	for i in output_pins:
+		GPIO.output(i,GPIO.LOW)
+	# if(lampu1 == "1"):
+	# 	GPIO.output(output_pins[0],GPIO.HIGH)
+	# 	GPIO.output(output_pins[1],GPIO.HIGH)
+	# 	GPIO.output(output_pins[2],GPIO.HIGH)
+	# 	GPIO.output(output_pins[3],GPIO.HIGH)
+	# elif(lampu1 == "2"):
+	# 	GPIO.output(output_pins[4],GPIO.HIGH)
+	# 	GPIO.output(output_pins[5],GPIO.HIGH)
+	# 	GPIO.output(output_pins[1],GPIO.HIGH)
+	# 	GPIO.output(output_pins[0],GPIO.HIGH)
+	# elif(lampu1 == "3"):
+	# 	GPIO.output(output_pins[0],GPIO.HIGH)
+	# 	GPIO.output(output_pins[0],GPIO.HIGH)
+	# 	GPIO.output(output_pins[0],GPIO.HIGH)
+	# 	GPIO.output(output_pins[0],GPIO.HIGH)
+	# else:
+	# 	print("merah")
 
 	time.sleep(0.5)
 		
